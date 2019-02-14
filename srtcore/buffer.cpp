7a060a6a7423fea3b8e6646647fd2db9b46a3c1e
Maxim Sharabayko <maxlovic@gmail.com>
2019-02-14 13:03:28 +0100

    Deleted logging

--- srtcore/buffer.cpp
+++ srtcore/buffer.cpp
@@ -201,9 +201,7 @@ void CSndBuffer::addBuffer(const char* data, int len, int ttl, bool order, uint6
     CGuard::enterCS(m_BufLock);
     m_iCount += size;
 
-    cerr << "CSndBuffer::addBuffer() m_iBytesCount " << m_iBytesCount << " + " << len;
     m_iBytesCount += len;
-    cerr << " = " << m_iBytesCount << endl;
     m_ullLastOriginTime_us = time;
 
     updInputRate(time, size, len);
@@ -341,9 +339,7 @@ int CSndBuffer::addBufferFromFile(fstream& ifs, int len)
 
    CGuard::enterCS(m_BufLock);
    m_iCount += size;
-   cerr << "CSndBuffer::addBufferFromFile() m_iBytesCount " << m_iBytesCount << " + " << total;
    m_iBytesCount += total;
-   cerr << " = " << m_iBytesCount << endl;
 
    CGuard::leaveCS(m_BufLock);
 
@@ -485,8 +481,6 @@ void CSndBuffer::ackData(int offset)
    CGuard bufferguard(m_BufLock);
 
    bool move = false;
-    cerr << "CSndBuffer::ackData() -" << offset << " bytes (";
-    cerr << m_iBytesCount;
    for (int i = 0; i < offset; ++ i)
    {
       m_iBytesCount -= m_pFirstBlock->m_iLength;
@@ -494,7 +488,6 @@ void CSndBuffer::ackData(int offset)
           move = true;
       m_pFirstBlock = m_pFirstBlock->m_pNext;
    }
-    cerr << " -> " << m_iBytesCount << ")" << endl;
    if (move)
        m_pCurrBlock = m_pFirstBlock;
 
@@ -598,9 +591,7 @@ int CSndBuffer::dropLateData(int &bytes, uint64_t latetime)
    if (move) m_pCurrBlock = m_pFirstBlock;
    m_iCount -= dpkts;
 
-   cerr << "Dropping " << dbytes << " bytes (" << m_iBytesCount;
    m_iBytesCount -= dbytes;
-   cout << " -> " << m_iBytesCount << endl;
    bytes = dbytes;
 
 #ifdef SRT_ENABLE_SNDBUFSZ_MAVG
