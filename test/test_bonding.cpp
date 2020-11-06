91a8a9043e2d27e3a77d11bcfbf004d914839d6e
Mikołaj Małecki <mmalecki@haivision.com>
2020-11-06 09:34:04 +0100

    Fixed warning in test

--- test/test_bonding.cpp
+++ test/test_bonding.cpp
@@ -80,7 +80,7 @@ void listening_thread()
     // srt_accept..
 }
 
-void ConnectCallback(void* opaq, SRTSOCKET sock, int error, const sockaddr* /*peer*/, int token)
+void ConnectCallback(void* /*opaq*/, SRTSOCKET sock, int error, const sockaddr* /*peer*/, int token)
 {
     std::cout << "Connect callback. Socket: " << sock
         << ", error: " << error
