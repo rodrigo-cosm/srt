9287feab55dea0197de34e7a51d003afa36140da
Mikołaj Małecki <mmalecki@haivision.com>
2023-09-27 17:05:18 +0200

    Fixed: strings with NUL inside were not properly handled

--- srtcore/socketconfig.h
+++ srtcore/socketconfig.h
@@ -152,7 +152,7 @@ public:
 
     std::string str() const
     {
-        return len == 0 ? std::string() : std::string(stor);
+        return len == 0 ? std::string() : std::string(stor, len);
     }
 
     const char* c_str() const
